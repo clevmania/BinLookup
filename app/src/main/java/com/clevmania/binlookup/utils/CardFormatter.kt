411666d2f109a3e705427bb2de32805bb490b095
clevmania <ernestcleverest@yahoo.com>
2020-12-22 21:23:39 +0100

    fix: support editing number at any position

--- app/src/main/java/com/clevmania/binlookup/utils/CardFormatter.kt
+++ app/src/main/java/com/clevmania/binlookup/utils/CardFormatter.kt
@@ -1,7 +1,6 @@
 package com.clevmania.binlookup.utils
 
 import android.text.Editable
-import android.text.TextUtils
 import android.text.TextWatcher
 
 
@@ -11,25 +10,28 @@ import android.text.TextWatcher
  */
 
 class CardFormatter : TextWatcher {
+
     override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
     override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
     override fun afterTextChanged(s: Editable) {
-        if (s.isNotEmpty() && s.length % 5 == 0) {
-            val c = s[s.length - 1]
-            if (space == c) {
-                s.delete(s.length - 1, s.length)
+        var source = s.toString()
+        if (lastSource != source) {
+            source = source.replace(SPACE, EMPTY_STRING)
+            val stringBuilder = StringBuilder()
+            for (i in source.indices) {
+                if (i > 0 && i % 4 == 0) {
+                    stringBuilder.append(SPACE)
+                }
+                stringBuilder.append(source[i])
             }
-        }
-
-        if (s.isNotEmpty() && s.length % 5 == 0) {
-            val c = s[s.length - 1]
-            if (Character.isDigit(c) && TextUtils.split(s.toString(), space.toString()).size <= 3) {
-                s.insert(s.length - 1, space.toString())
-            }
+            lastSource = stringBuilder.toString()
+            s.replace(0, s.length, lastSource)
         }
     }
 
     companion object {
-        private const val space = ' '
+        private const val EMPTY_STRING = ""
+        private const val SPACE = " "
+        private var lastSource = EMPTY_STRING
     }
 }
